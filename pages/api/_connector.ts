de4c415118c3b5a7a3acd9a540a577b89ea73401
djfusco <davidjfusco@gmail.com>
2022-03-28 12:27:12 -0400

    removed old params

--- pages/api/_connector.ts
+++ pages/api/_connector.ts
@@ -6,7 +6,7 @@ export async function connectToDatabase() {
   if (cachedDb) {
     return cachedDb;
   }
-  const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new MongoClient(process.env.MONGODB_URI);
 
   cachedDb = client;
   return await client.connect();
