75c5c56acaa7a12f01320864b110b636e265dc85
djfusco <davidjfusco@gmail.com>
2022-04-01 15:39:04 -0400

    added cors settings
    cors settings

--- pages/api/shorten_link.ts
+++ pages/api/shorten_link.ts
@@ -4,6 +4,11 @@ var hash = require('object-hash');
 
 
 export default async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+
   const db = await connectToDatabase();
 
   if (req.body !== '' && req.body.link !== undefined && req.body.link !== '') {
